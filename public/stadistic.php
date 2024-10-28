6e69dad74549789f9046baa4c069a7809251e8f3
Andrés Crespo <acrerosj@canariaseducacion.es>
2024-10-28 17:15:36 +0000

    stadistic

--- public/stadistic.php
+++ public/stadistic.php
@@ -4,16 +4,59 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+  <h1>Estadísticas</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>
+          Fecha
+        </th>
+        <th>
+          Hora
+        </th>
+        <th>
+          Cantidad
+        </th>
+        <th>
+          Media
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+
+
   <?php
-    $fileName = '..\data\2024_10_25_15_15.csv';
-    $content = file_get_contents($fileName);
-    $rates = explode(',', $content);
-    array_pop($rates);
-    // $count =
-    // $total = 
-    // $avg = $total / $count; 
+    $path = '../data/';
+    $dir = opendir($path);
+    while ($fileName = readdir($dir)) {
+      // lee cada archivo del directorio.
+      //echo $fileName . "<br>";
+      if (is_file($path . $fileName)) {
+        $fileNameWithoutExtension = explode('.', $fileName)[0];
+        list($year, $month, $day, $hour, $minute) = explode('_', $fileNameWithoutExtension);
+        $date = $year . "/" . $month . "/" . $day;
+        $time = $hour . ":" . $minute;
+        $content = file_get_contents($path . $fileName);
+        $rates = explode(',', $content);
+        array_pop($rates);
+        $count = count($rates);
+        $total = 0;
+        foreach ($rates as $rate) {
+          $total += $rate;
+        }
+        $avg = $total / $count; 
+        printf("<tr><td>%s</td><td>%s</td><td>%d</td><td>%.2f</td></tr>",
+          $date, $time, $count, $avg);
+      }
+    }
+    closedir($dir);
+ 
   ?>
+
+  </tbody>
+</table>
 </body>
 </html>
